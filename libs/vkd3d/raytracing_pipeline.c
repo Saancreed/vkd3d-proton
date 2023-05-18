66c3ca82dced617b247f8326069bb09191fc7323
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2023-05-18 02:42:43 +0200

    vkd3d: Implement ID3D12DeviceExt5_SetCreatePipelineStateOptions.
    Signed-off-by: Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>

--- libs/vkd3d/raytracing_pipeline.c
+++ libs/vkd3d/raytracing_pipeline.c
@@ -2536,6 +2536,8 @@ static HRESULT d3d12_state_object_compile_pipeline_variant(struct d3d12_rt_state
     if (object->pipeline_config.Flags & D3D12_RAYTRACING_PIPELINE_FLAG_ALLOW_OPACITY_MICROMAPS)
         pipeline_create_info.flags |= VK_PIPELINE_CREATE_RAY_TRACING_OPACITY_MICROMAP_BIT_EXT;
 
+    pipeline_create_info.flags |= object->device->global_ray_tracing_pipeline_create_flags;
+
     library_info.sType = VK_STRUCTURE_TYPE_PIPELINE_LIBRARY_CREATE_INFO_KHR;
     library_info.pNext = NULL;
     library_info.libraryCount = data->vk_libraries_count;
--- libs/vkd3d/vkd3d_private.h
+++ libs/vkd3d/vkd3d_private.h
@@ -5730,6 +5730,8 @@ struct d3d12_device
     LUID adapter_luid;
     D3DKMT_HANDLE kmt_local;
 
+    VkPipelineCreateFlags global_ray_tracing_pipeline_create_flags;
+
     struct vkd3d_private_store private_store;
     struct d3d_destruction_notifier destruction_notifier;
     struct d3d12_caps d3d12_caps;
